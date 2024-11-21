3401c0f685195a968cea6b2794f11403dfb8f082
Vitor <vitormarinheiro9@gmail.com>
2024-11-20 21:21:54 -0300

    feat: add validacoes na pagina de registro

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,6 +16,19 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+        ], [
+            'name.required' => 'O campo nome é obrigatório.',
+            'email.required' => 'O campo e-mail é obrigatório.',
+            'email.email' => 'O campo e-mail deve ser um endereço de e-mail válido.',
+            'email.unique' => 'Já existe um usuário com este e-mail.',
+            'password.required' => 'O campo senha é obrigatório.',
+            'password.min' => 'A senha deve ter pelo menos 6 caracteres.',
+        ]);
+
         $data = $request->except(['_token']);
         $data['password'] = Hash::make($data['password']);
 
@@ -29,7 +42,7 @@ class UsersController extends Controller
     public function destroy()
     {
         Auth::logout();
-        
+
         return to_route('login');
     }
 }
